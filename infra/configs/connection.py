d5088a0eadd6bc87c1a05c1e92a0b8fc73f3504f
titionesenac <titione.amorim@prof.sc.senac.br>
2023-05-03 16:42:34 -0300

    Att

--- infra/configs/connection.py
+++ infra/configs/connection.py
@@ -3,20 +3,21 @@ from sqlalchemy.orm import sessionmaker
 
 from infra.configs.base import Base
 
+
 class DBConnectionHandler:
 
     def __init__(self):
-        #Dados de endereço do banco de dados
+        # Dados de endereço do banco de dados
         self.__connection_string = 'mysql+pymysql://admin:admin@localhost:3306/notas'
-        #Instância do engine(gerenciador do banco)
+        # Instância do engine(gerenciador do banco)
         self.__engine = self.__create_database_engine()
-        #Sessão nula para que possa ser alocada uma nova ao ser instanciado um obj
+        # Sessão nula para que possa ser alocada uma nova ao ser instanciado um obj
         self.session = None
-        #Validação de existência de banco de dados ao instanciar um obj
+        # Validação de existência de banco de dados ao instanciar um obj
         self.__create_database()
 
-    #Método para validação da existencia do banco de dados, caso
-    #não exista realiza criação
+    # Método para validação da existencia do banco de dados, caso
+    # não exista realiza criação
     def __create_database(self):
         engine = create_engine(self.__connection_string, echo=True)
         try:
@@ -38,8 +39,8 @@ class DBConnectionHandler:
 
         print("Tabela criada com sucesso!")
 
-    #Função para crição da engine sem necessidade de informar dados de endereço do banco
-    #e utilização de queryes escritas à mão
+    # Função para crição da engine sem necessidade de informar dados de endereço do banco
+    # e utilização de queryes escritas à mão
     def __create_database_engine(self):
         engine = create_engine(self.__connection_string)
         return engine
@@ -47,7 +48,7 @@ class DBConnectionHandler:
     def get_engine(self):
         return self.__engine
 
-    #Funções mágicas que definem qualquer comportamento ao serem geradas instâncias
+    # Funções mágicas que definem qualquer comportamento ao serem geradas instâncias
     def __enter__(self):
         session_make = sessionmaker(bind=self.__engine)
         print('Gerando conexão')
@@ -57,4 +58,3 @@ class DBConnectionHandler:
     def __exit__(self, exc_type, exc_val, exc_tb):
         print('Fechando conexão')
         self.session.close()
-
--- infra/repository/nota_repository.py
+++ infra/repository/nota_repository.py
@@ -1,15 +1,16 @@
 from infra.configs.connection import DBConnectionHandler
 from infra.entities.nota import Nota
 
+
 class NotaRepository:
 
-    #Método para realizar a consulta de todas as notas
+    # Método para realizar a consulta de todas as notas
     def select_all(self):
         with DBConnectionHandler() as db:
             data = db.session.query(Nota).all()
             return data
 
-    #Método para inserir nota no banco de dados
+    # Método para inserir nota no banco de dados
     def insert(self, titulo, texto, prioridade, data_criacao):
         with DBConnectionHandler() as db:
             data_insert = Nota(titulo=titulo, texto=texto, prioridade=prioridade,
@@ -17,16 +18,16 @@ class NotaRepository:
             db.session.add(data_insert)
             db.session.commit()
 
-    #Método para realizar a remoção de uma nota do banco de dados
+    # Método para realizar a remoção de uma nota do banco de dados
     def delete(self, id):
         with DBConnectionHandler() as db:
             db.session.query(Nota).filter(Nota.id == id).delete()
             db.session.commit()
 
-    #Método para atualizar uma nota
+    # Método para atualizar uma nota
     def update(self, id, titulo, texto, prioridade):
         with DBConnectionHandler() as db:
-            db.session.query(Nota).filter(Nota.id == id)\
-                .update({'titulo' : titulo, 'texto' : texto,
-                         'prioridade' : prioridade})
+            db.session.query(Nota).filter(Nota.id == id) \
+                .update({'titulo': titulo, 'texto': texto,
+                         'prioridade': prioridade})
             db.session.commit()
